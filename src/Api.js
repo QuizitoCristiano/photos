8504978b7ffea65e84a732827dfb9c9651ae22c7
Quizito Cristiano <quizitocristiano10@gmail.com>
2022-07-12 17:38:12 -0300

    formata arquivos

--- src/Api.js
+++ src/Api.js
@@ -1,30 +1,30 @@
-import "firebase/auth";
-import "firebase/firestore";
-import "firebase/storage"; // <----
-import firebaseconfig from "./firebaseconfig";
+import firebase from 'firebase/app'
 
-const firebaseApp = firebase.initializeApp(firebaseconfig)
-const db = firebaseApp.firestore();
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage' // <----
+import { firebaseConfig } from './firebaseConfig'
 
-export const storage = firebase.storage().ref();
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+
+export const storage = firebase.storage().ref()
 
 export default {
     createUser: async (u) => {
-
-        return await db.collection("users").doc(u.id).set(
+        return await db.collection('users').doc(u.id).set(
             {
                 name: u.name,
                 email: u.email,
                 password: u.password,
             },
             { merge: true }
-        );
+        )
     },
     listUsers: async () => {
-        let list = [];
-        let results = await db.collection("users").get();
-        results.forEach((user) => list.push(user.data()));
-        return list;
-    }
+        let list = []
+        let results = await db.collection('users').get()
+        results.forEach((user) => list.push(user.data()))
+        return list
+    },
 }
-
--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -1,19 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export default {
-  apiKey: "AIzaSyAdsuH3gfo_Nmw_r2NUQOwf650i6BBwjeM",
-  authDomain: "app-photos-7ef7e.firebaseapp.com",
-  projectId: "app-photos-7ef7e",
-  storageBucket: "app-photos-7ef7e.appspot.com",
-  messagingSenderId: "824513536053",
-  appId: "1:824513536053:web:b7ee06daf7cb5889c7517c",
-  measurementId: "G-13CJCZTP3H"
-};
-
-
+export const firebaseConfig = {
+    apiKey: 'AIzaSyAdsuH3gfo_Nmw_r2NUQOwf650i6BBwjeM',
+    authDomain: 'app-photos-7ef7e.firebaseapp.com',
+    projectId: 'app-photos-7ef7e',
+    storageBucket: 'app-photos-7ef7e.appspot.com',
+    messagingSenderId: '824513536053',
+    appId: '1:824513536053:web:b7ee06daf7cb5889c7517c',
+    measurementId: 'G-13CJCZTP3H',
+}
