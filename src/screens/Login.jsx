4f4a561c0f2d83fb562450d98d4c2dd6b61f069a
Quizito Cristiano <quizitocristiano10@gmail.com>
2022-07-12 17:37:57 -0300

    formata arquivos

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,13 +1,13 @@
-import { MainLayout } from "../layouts/MainLayout"
+import { MainLayout } from '../layouts/MainLayout'
 import styles from '../styles/login.module.css'
-import Api from "../Api"
-
+import Api from '../Api'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 
 import Email from '../imagem/emeil.png'
 import Password from '../imagem/password.png'
-import { useEffect, useState } from "react"
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
-import { async } from "@firebase/util"
+import { useEffect, useState } from 'react'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 
 export const LoginScreen = (props) => {
     const [user, setUser] = useState()
@@ -16,67 +16,94 @@ export const LoginScreen = (props) => {
     const handleChange = (e) => {
         const value = e.target.value
         const name = e.target.name
-        
-        setUser((estadoAnterior)=> {
-        return {
-            ...estadoAnterior,
-            [name]: value
-        }
+
+        setUser((estadoAnterior) => {
+            return {
+                ...estadoAnterior,
+                [name]: value,
+            }
         })
     }
-    const handleSubmit = async () => {
-        const users = await Api.listUsers();
-        console.log(users)
-    }
-    // console.log('user', user)
     // const handleSubmit = async () => {
-    //     const auth = getAuth();
-    //     try {
-    //         const response = await signInWithEmailAndPassword(auth, user?.email, user?.password)
-    //     } catch(e) {
-    //         setWrongUser(true)
-    //       }
-        
-        
+    //     const users = await Api.listUsers();
+    //     console.log(users)
     // }
-    // useEffect(()=>{
-    //     setTimeout(()=> setWrongUser(false), 3000)
-    // }, [wrongUser])
+    console.log('user', user)
+    const handleSubmit = async () => {
+        try {
+            const res = await firebase
+                .auth()
+                .signInWithEmailAndPassword(user?.email, user?.password)
+        } catch (e) {
+            setWrongUser(true)
+        }
+    }
 
-    // function handleSubmit() {
-    //     window.location.href="http://localhost:3000/discover"
-    // }
-  
+    useEffect(() => {
+        setTimeout(() => setWrongUser(false), 3000)
+    }, [wrongUser])
+
     function catatro() {
-        window.location.href="http://localhost:3000/register"
-      }
+        window.location.href = 'http://localhost:3000/register'
+    }
+
     return (
-    <MainLayout color="blue">
-     <div className="content">
-        <h1>Login</h1>
-        <br/><br/>
-        <div className="input-container">
-        <img className=" imagem-input" src={Email} height="60" alt="imagem login"/>
-        
-        <input id="inputEmail" name="email" onChange={handleChange}  className={styles.input} type="text" placeholder="E-mail"/>
-       
-        </div>
-        
-        <br/><br/>
-        <div className="input-container">
-        <img className="imagem-input "src={Password} height="60" alt="imagem login"/>
-        <input id="Senha" name="password" onChange={handleChange}  className={styles.input} type="password" placeholder="Senha"/>
-        </div>
+        <MainLayout color="blue">
+            <div className="content">
+                <h1>Login</h1>
+                <br />
+                <br />
+                <div className="input-container">
+                    <img
+                        className=" imagem-input"
+                        src={Email}
+                        height="60"
+                        alt="imagem login"
+                    />
 
+                    <input
+                        id="inputEmail"
+                        name="email"
+                        onChange={handleChange}
+                        className={styles.input}
+                        type="text"
+                        placeholder="E-mail"
+                    />
+                </div>
 
-        <br/><br/>
-        { wrongUser && <div>email ou senha incorretos! Tente novamente</div> }
-        <div className={styles.Divbotao}>
-            <button onClick={handleSubmit} className={styles.botalogi} >Entrar</button>
-            <button className={styles.catastro} onClick={catatro}>Catastro</button>
-        </div>
-       
-    </div>
-    </MainLayout>
+                <br />
+                <br />
+                <div className="input-container">
+                    <img
+                        className="imagem-input "
+                        src={Password}
+                        height="60"
+                        alt="imagem login"
+                    />
+                    <input
+                        id="Senha"
+                        name="password"
+                        onChange={handleChange}
+                        className={styles.input}
+                        type="password"
+                        placeholder="Senha"
+                    />
+                </div>
+
+                <br />
+                <br />
+                {wrongUser && (
+                    <div>email ou senha incorretos! Tente novamente</div>
+                )}
+                <div className={styles.Divbotao}>
+                    <button onClick={handleSubmit} className={styles.botalogi}>
+                        Entrar
+                    </button>
+                    <button className={styles.catastro} onClick={catatro}>
+                        Catastro
+                    </button>
+                </div>
+            </div>
+        </MainLayout>
     )
 }
--- src/screens/Register.jsx
+++ src/screens/Register.jsx
@@ -1,79 +1,108 @@
-import { MainLayout } from "../layouts/MainLayout"
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-
+import { MainLayout } from '../layouts/MainLayout'
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
 
 import '../styles/registro.css'
-
-
 
 import Senha from '../imagem/senhas.png'
 import Senhas from '../imagem/senha.png'
 import Email from '../imagem/email.png'
 
-import { useState } from "react"
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 export const RegisterScreen = () => {
-  const [user, setUser] = useState()
-  const [wrongUser, setWrongUser] = useState(false)
+    const [user, setUser] = useState()
+    const [wrongUser, setWrongUser] = useState(false)
 
-  const handleChange = (e) => {
-    const value = e.target.value
-    const name = e.target.name
-    
-    setUser((estadoAnterior)=> {
-      return {
-        ...estadoAnterior,
-        [name]: value
-      }
-    })
-  }
+    const handleChange = (e) => {
+        const value = e.target.value
+        const name = e.target.name
 
-  const handleSubmit = async () => {
-    const auth = getAuth();
-    try {
-      const response = await createUserWithEmailAndPassword(auth, user?.email, user?.password)
-    } catch(e) {
-      setWrongUser(true)
+        setUser((estadoAnterior) => {
+            return {
+                ...estadoAnterior,
+                [name]: value,
+            }
+        })
     }
-    
-  }
 
- 
+    // // const handleSubmit = async () => {
+    // //   const auth = getAuth();
+    // //   try {
+    // //     const response = await createUserWithEmailAndPassword(auth, user?.email, user?.password)
+    // //   } catch(e) {
+    // //     setWrongUser(true)
+    // //   }
 
-  function login(){
-    window.location.href="http://localhost:3000/login"
-  }
- 
-  return (
-    <MainLayout>
-      
-        <div onClick={login} className="seta">
-          <svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fillRule="evenodd" clipRule="evenodd"
-            d="M1.91421 4.35355L4.56066 7L3.85355 7.70711L0.353553 4.20711L0 3.85355L0.353553 3.5L3.85355 0L4.56066 0.707107L1.91421 3.35355L5.70711 3.35355C9.28325 3.35355 12.2071 6.27741 12.2071 9.85355L12.2071 11.3536H11.2071L11.2071 9.85355C11.2071 6.8297 8.73096 4.35355 5.70711 4.35355H1.91421Z"
-            fill="black" fillOpacity="0.8" />
-          </svg>
-        </div>
+    // }
 
-        <h2>Register</h2>
-        <br/><br/>
-        <div className="input-container">
-          <img className="imagem-input" src={Senha} height="60" alt="" />
-          <input name="username" className="impute" type="text" placeholder="Nome" onChange={handleChange}/>
-        </div>
-        <br/><br/>
-        <div className="input-container">
-          <img className="imagem-input" src={Email} height="60" alt="" />
-          <input name="email" className="impute" type="text" placeholder="E-mail" onChange={handleChange} />
-        </div>
-        <br/><br/>
-        <div className="input-container">
-          <img className="imagem-input" src={Senhas} height="60" alt="" />
-          <input name="password" className="impute" type="password" placeholder="Senha" onChange={handleChange}/>
-        </div>
-        
-        { wrongUser && <div>email ou senha incorretos! Tente novamente</div> }
-        <br/><br/>
-        <button onClick={handleSubmit} className="mae">Register</button>        
-    </MainLayout>
-  )
+    function handleSubmit() {
+        window.location.href = 'http://localhost:3000/login'
+    }
+
+    return (
+        <MainLayout>
+            <Link to="/login">
+                <svg
+                    width="13"
+                    height="12"
+                    viewBox="0 0 13 12"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M1.91421 4.35355L4.56066 7L3.85355 7.70711L0.353553 4.20711L0 3.85355L0.353553 3.5L3.85355 0L4.56066 0.707107L1.91421 3.35355L5.70711 3.35355C9.28325 3.35355 12.2071 6.27741 12.2071 9.85355L12.2071 11.3536H11.2071L11.2071 9.85355C11.2071 6.8297 8.73096 4.35355 5.70711 4.35355H1.91421Z"
+                        fill="black"
+                        fillOpacity="0.8"
+                    />
+                </svg>
+            </Link>
+
+            <h2>Register</h2>
+            <br />
+            <br />
+            <div className="input-container">
+                <img className="imagem-input" src={Senha} height="60" alt="" />
+                <input
+                    name="username"
+                    className="impute"
+                    type="text"
+                    placeholder="Nome"
+                    onChange={handleChange}
+                />
+            </div>
+            <br />
+            <br />
+            <div className="input-container">
+                <img className="imagem-input" src={Email} height="60" alt="" />
+                <input
+                    name="email"
+                    className="impute"
+                    type="text"
+                    placeholder="E-mail"
+                    onChange={handleChange}
+                />
+            </div>
+            <br />
+            <br />
+            <div className="input-container">
+                <img className="imagem-input" src={Senhas} height="60" alt="" />
+                <input
+                    name="password"
+                    className="impute"
+                    type="password"
+                    placeholder="Senha"
+                    onChange={handleChange}
+                />
+            </div>
+
+            {wrongUser && <div>email ou senha incorretos! Tente novamente</div>}
+            <br />
+            <br />
+            <button onClick={handleSubmit} className="mae">
+                Register
+            </button>
+        </MainLayout>
+    )
 }
